130accb8d91ac69094caac18a63c6f4ee7702dbb
Nguyen Cong Thu <congthu137@gmail.com>
2022-08-22 16:29:08 +0700

    make part of uilabel bold

---
+++ contact/Controller/Home/HomeCell/HomeCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeCell.swift
+//  contact
+//
+//  Created by Nguyễn Công Thư on 22/08/2022.
+//
+
+import UIKit
+
+class HomeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
